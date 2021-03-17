8e989bbfa7927306c99a706db4d7c9b9a6da1b99
bigdesire0210 <bigdesire210@gmail.com>
2021-03-17 18:27:32 +0900

    じゃんけんゲーム一応完成

--- じゃんけん/main.js
+++ じゃんけん/main.js
@@ -1,9 +1,14 @@
 function Janken() {
-    myNumber = document.getElementById('myHand').selectedIndex;
+    const myNumber = document.getElementById('myHand').selectedIndex;
+
     Gu = document.getElementById('gu');
     Tyoki = document.getElementById('tyoki');
     Pa = document.getElementById('pa');
-    cpuNumber = Math.floor(Math.random() * 3);
+
+    const cpuNumber = Math.floor(Math.random() * 3);
+
+    let result = "";
+    let cpuString = "";
 
     Gu = 0;
     Tyoki = 1;
@@ -12,58 +17,75 @@ function Janken() {
     // let result;
 
     //グーの定義
-    if (cpuNumber == 0) {
-        cpuNumber = "グー";
+    if (cpuNumber === 0) {
+        cpuString = "グー";
     }
     //チョキの定義
-    if (cpuNumber == 1) {
-        cpuNumber = "チョキ";
+    if (cpuNumber === 1) {
+        cpuString = "チョキ";
     }
     //パーの定義
-    if (cpuNumber == 2) {
-        cpuNumber = "パー";
+    if (cpuNumber === 2) {
+        cpuString = "パー";
     }
 
     //勝った時
-    if (Gu - cpuNumber == -1) {
-        result = "WIN!! おめでとう！";
-    }
-
-    if (Tyoki - cpuNumber == -1) {
-        result = "WIN!! おめでとう！";
-    }
-
-    if (Pa - cpuNumber == 2) {
+    if (myNumber - cpuNumber === -1) {
         result = "WIN!!";
     }
 
+    if (myNumber - cpuNumber === 2) {
+        result = "WIN!!";
+    }
+    // if (Gu - cpuNumber === -1) {
+    //     result = "WIN!! おめでとう！";
+    // }
+
+    // if (Tyoki - cpuNumber === -1) {
+    //     result = "WIN!! おめでとう！";
+    // }
+
+    // if (Pa - cpuNumber === 2) {
+    //     result = "WIN!!";
+    // }
+
     //あいこの時
-    if (Gu - cpuNumber == 0) {
-        result = "引き分けだよ、もう一回";
+    if (myNumber - cpuNumber === 0) {
+        result = "引き分け！";
     }
+    // if (Gu - cpuNumber === 0) {
+    //     result = "引き分けだよ、もう一回";
+    // }
 
-    if (Tyoki - cpuNumber == 0) {
-        result = "引き分け";
-    }
+    // if (Tyoki - cpuNumber === 0) {
+    //     result = "引き分け";
+    // }
 
-    if (Pa - cpuNumber == 0) {
-        result = "引き分け";
-    }
+    // if (Pa - cpuNumber === 0) {
+    //     result = "引き分け";
+    // }
 
     //負けた時
-    if (Gu - cpuNumber == -2) {
-        result = "LOSE!!　残念！";
+    if (myNumber - cpuNumber === -2) {
+        result = "LOSE...";
     }
 
-    if (Tyoki - cpuNumber == -1) {
-        result = "LOSE!! 残念！";
+    if (myNumber - cpuNumber === 1) {
+        result = "LOSE...";
     }
+    // if (Gu - cpuNumber === -2) {
+    //     result = "LOSE!!　残念！";
+    // }
 
-    if (Pa - cpuNumber == 2) {
-        result = "LOSE";
-    }
+    // if (Tyoki - cpuNumber === 1) {
+    //     result = "LOSE!! 残念！";
+    // }
 
-    document.getElementById("message1").innerHTML = "あいての手" + cpuNumber;
+    // if (Pa - cpuNumber === 1) {
+    //     result = "LOSE";
+    // }
+
+    document.getElementById("message1").innerHTML = "あいての手" + cpuString;
 
     document.getElementById("message2").innerHTML = "結果" + result;
 
