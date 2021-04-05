a952e761022e783213ab92e5f78ca634246aa35c
bigdesire0210 <bigdesire210@gmail.com>
2021-04-05 14:57:10 +0900

    サイコロ立体的実装

--- サイコロ/main.js
+++ サイコロ/main.js
@@ -1,8 +1,30 @@
-Saikoro = new Array("saikoro1.png", "saikoro2.png", "saikoro3.png",
-    "saikoro4.png", "saikoro5.png", "saikoro6.png");
+// Saikoro = new Array("rittai1.png", "rittai2.png", "rittai3.png",
+//     "rittai4.png", "rittai5.png", "rittai6.png", "rittai0.gif");
 
-function change() {
-    change = Math.random() * 6;
-    Change = Math.floor(change) + 1;
-    document.saikoro.src = Saikoro[Change];
+Saikoro = [];
+
+function getDice1() {
+    const change = Math.floor(Math.random() * 6);
+    const number = change + 1;
+    document.getElementById("innerDice").innerHTML =
+        '<img src="./illust/rittai' + number + '.png" width="400">';
+}
+
+
+
+function change(t) {
+
+
+    if (t.id == "start") {
+        Saikoro.push(setInterval(getDice1, 50));
+        console.log(Saikoro);
+    }
+
+    if (t.id == "stop") {
+        console.log(Saikoro);
+        clearInterval(Saikoro.shift());
+    }
+    // random = Math.random() * 6;
+    // Change = Math.floor(random) + 1;
+    // document.saikoro.src = Saikoro[Change];
 } 
